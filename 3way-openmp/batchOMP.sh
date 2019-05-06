883c4db33e1967a72025ec93721e784f3736bb15
John Riforgiate <johnriforgiate@eos.beocat.ksu.edu>
2019-05-06 11:07:17 -0500

    Added more time to the batch command

--- 3way-openmp/batchOMP.sh
+++ 3way-openmp/batchOMP.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-sbatch --constraint=elves --time=1:30:00 --mem-per-cpu=4G --nodes=1 --cpus-per-task=1 ./proj4OMP.sh 1 /homes/johnriforgiate/cis520/wiki_dump.txt
-sbatch --constraint=elves --time=1:30:00 --mem-per-cpu=4G --nodes=1 --cpus-per-task=2 ./proj4OMP.sh 2 /homes/johnriforgiate/cis520/wiki_dump.txt
-sbatch --constraint=elves --time=1:30:00 --mem-per-cpu=4G --nodes=1 --cpus-per-task=4 ./proj4OMP.sh 4 /homes/johnriforgiate/cis520/wiki_dump.txt
-sbatch --constraint=elves --time=1:30:00 --mem-per-cpu=4G --nodes=1 --cpus-per-task=8 ./proj4OMP.sh 8 /homes/johnriforgiate/cis520/wiki_dump.txt
-sbatch --constraint=elves --time=1:30:00 --mem-per-cpu=4G --nodes=1 --cpus-per-task=16 ./proj4OMP.sh 16 /homes/johnriforgiate/cis520/wiki_dump.txt
+sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=4G --nodes=1 --cpus-per-task=1 ./proj4OMP.sh 1 /homes/johnriforgiate/cis520/wiki_dump.txt
+sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=4G --nodes=1 --cpus-per-task=2 ./proj4OMP.sh 2 /homes/johnriforgiate/cis520/wiki_dump.txt
+sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=4G --nodes=1 --cpus-per-task=4 ./proj4OMP.sh 4 /homes/johnriforgiate/cis520/wiki_dump.txt
+sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=4G --nodes=1 --cpus-per-task=8 ./proj4OMP.sh 8 /homes/johnriforgiate/cis520/wiki_dump.txt
+sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=4G --nodes=1 --cpus-per-task=16 ./proj4OMP.sh 16 /homes/johnriforgiate/cis520/wiki_dump.txt
