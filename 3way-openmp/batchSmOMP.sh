576f50d5fc6e352bd9612bb97b0be56b4f46b74f
John Riforgiate <johnriforgiate@eos.beocat.ksu.edu>
2019-05-06 21:56:35 -0500

    Added files to test smaller file sizes

---
+++ 3way-openmp/batchSmOMP.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+for i in 1 2 4 8 16
+do
+sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=4G --nodes=1 --cpus-per-task=$i ./proj4OMP.sh $i /homes/johnriforgiate/wiki_dump2.txt
+sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=4G --nodes=1 --cpus-per-task=$i ./proj4OMP.sh $i /homes/johnriforgiate/wiki_dump4.txt
+sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=4G --nodes=1 --cpus-per-task=$i ./proj4OMP.sh $i /homes/johnriforgiate/wiki_dump8.txt
+sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=4G --nodes=1 --cpus-per-task=$i ./proj4OMP.sh $i /homes/johnriforgiate/wiki_dump16.txt
+sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=4G --nodes=1 --cpus-per-task=$i ./proj4OMP.sh $i /homes/johnriforgiate/wiki_dump32.txt
+done
